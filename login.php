82161407ab6b4470ee2144944a16d2717cf08770
Mark Armendariz <marmendariz@cs.csubak.edu>
2016-02-05 13:12:03 -0800

    Small changes.

--- login.php
+++ login.php
@@ -1,9 +1,19 @@
+
 <?php
+
 if(empty($_SERVER["HTTPS"]) ||  $_SERVER["HTTPS"] != "on")
 {
         header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
             exit();
 }
+
+
+
+
+
+
+
+
 ?>
 <!doctype html>
 <html class="no-js" lang="en">
