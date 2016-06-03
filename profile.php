d8fc418538521d0c6b8ae0fbaa12de1afe102be1
kobrien <kobrien@sleipnir.cs.csubak.edu>
2016-06-03 16:58:32 -0700

    readded the statistics that got erased

--- profile.php
+++ profile.php
@@ -66,6 +66,62 @@ if(isset($_SESSION['valid_user'])){
         echo "Administrator";
     else
         echo "No Valid User Type Found";
+
+
+    $query = 'select com_count from user_com_count where user_name=?';
+    $stmt = $db->prepare($query);
+    $stmt->bind_param('s',$username);
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($com_count);
+    $stmt->fetch();
+    
+    $query = 'select dis_count from user_dis_count where user_name=?';
+    $stmt = $db->prepare($query);
+    $stmt->bind_param('s',$username);
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($dis_count);
+    $stmt->fetch();
+    
+    $query = 'select Comment_Upvotes, Comment_Downvotes  from user_com_uvdv where user_name=?';
+    $stmt = $db->prepare($query);
+    $stmt->bind_param('s',$username);
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($com_upvote, $com_downvote);
+    $stmt->fetch();
+    
+    $query = 'select Dis_Upvotes, Dis_Downvotes  from user_dis_uvdv where user_name=?';
+    $stmt = $db->prepare($query);
+    $stmt->bind_param('s',$username);
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($dis_upvote, $dis_downvote);
+    $stmt->fetch();
+
+    $query = 'select User_Com_Rating from user_com_rating_noavg where user_name=?';
+    $stmt = $db->prepare($query);
+    $stmt->bind_param('s',$username);
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($com_rating);
+    $stmt->fetch();
+
+    $query = 'select User_Dis_Rating from user_dis_rating_noavg where user_name=?';
+    $stmt = $db->prepare($query);
+    $stmt->bind_param('s',$username);
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($dis_rating);
+    $stmt->fetch();
+    
+
+    $up_count = $com_upvote + $dis_upvote;
+    $down_count = $com_downvote + $dis_downvote;
+    $total_rating = floor(($com_rating + $dis_rating)/2);
+    $stmt->close();
+    $db->close();
     
     echo "</h4>";
     echo "</div>";
@@ -83,6 +139,7 @@ if(isset($_SESSION['valid_user'])){
     echo "<div class='columns panel text-center  large-8 large-centered medium-8 medium-centered  small-10 small-centered '>";
     echo "  <h3 style='color: #008cbb' >Statistics: </h3>";
     echo "  <div class='text-left'>";
+    echo "      <h6>User Rating: $total_rating </h6>";
     echo "      <h6>Date Joined: $date_joined </h6>";
     echo "      <h6>Comment Count: $com_count </h6>";
     echo "      <h6>Discussion Count: $dis_count </h6>";
